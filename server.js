963804f97fa486c1aabd1f430cd40d95978bc2a6
Manav <aroramanav89@gmail.com>
2022-11-21 00:24:12 -0500

    i

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 04
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name: manav Student ID:153341219 Date: 21 November 2022
+ *
+ *  Online (Cyclic) Link: https://splendid-yak-onesies.cyclic.app/
+ *
+ *******************************************************************************/
+var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require("path");
